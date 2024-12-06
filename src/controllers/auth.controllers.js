2db789be943924affaa1e3a90c0747761445b205
Giuseppi05 <giuseppi0501@gmail.com>
2024-12-05 22:15:20 -0500

    sgsdgfdgd

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -46,7 +46,6 @@ export const login = async (req, res) => {
 
         const token = await createAccessToken({ id: userFound._id });
         res.cookie('token', token, {
-            httpOnly: true,
             secure: true,
             sameSite: 'lax', 
         });
