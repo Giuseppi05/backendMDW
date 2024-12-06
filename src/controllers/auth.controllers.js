ba262d2e055f35c623594a0e7fb064f8003e4ae0
Giuseppi05 <giuseppi0501@gmail.com>
2024-12-06 15:53:36 -0500

    last commit

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -57,12 +57,12 @@ export const login = async (req, res) => {
         }
 
         res.cookie("token", token
-        //     , {
-        //     httpOnly: true,
-        //     secure: true, 
-        //     sameSite: "none", 
-        //     maxAge: 1000 * 60 * 60 * 24, 
-        //   }
+            , {
+            httpOnly: true,
+            secure: true, 
+            sameSite: "none", 
+            maxAge: 1000 * 60 * 60 * 24, 
+          }
         );
           
 
